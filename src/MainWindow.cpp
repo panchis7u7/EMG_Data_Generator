12ba14092e0ba6f35eb362881dcf329281aba022
Carlos Sebastian Madrigal Rodriguez <smadrigal.rod@gmail.com>
2022-05-07 12:44:17 -0500

    Generated initial proyect structure.

---
+++ src/MainWindow.cpp
@@ -0,0 +1,14 @@
+#include "ui/MainWindow/ui_MainWindow.h"
+#include <include/MainWindow.hpp>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
